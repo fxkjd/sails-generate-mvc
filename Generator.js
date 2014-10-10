cf0033d16f7ca683421ff37f7b73fd0774aba988
Eric Ponce <ponceeric@gmail.com>
2014-10-10 12:54:57 +0200

    Added simple index view

--- Generator.js
+++ Generator.js
@@ -96,7 +96,10 @@ module.exports = {
     // Then the file is copied into the specified destination (on the left).
     './api/controllers/:controllerFilename.js': { template: {templatePath: './api/controllers/controller.template.js', force: true}  },
     './api/models/:nameC.js': { template: {templatePath: './api/models/model.template.js', force: true}  },
-
+    
+    //CREATE VIEWS
+    './views/:nameC/index.ejs': { template: {templatePath: './views/index.template.js', force: true}  },
+    
     // Creates a folder at a static path
     //'./hey_look_a_folder': { folder: {} }
 
