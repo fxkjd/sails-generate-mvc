b28cac6a1bf338b1744f92654e217fd8b763237f
fxkjd <6142041+fxkjd@users.noreply.github.com>
2015-01-04 17:29:31 +0100

    add array and json attr on testing

--- templates/test/fixtures/model.json.template.js
+++ templates/test/fixtures/model.json.template.js
@@ -1,7 +1,7 @@
 [
   {
 
-    <% for(var i in attributes) { %>"<%=attributes[i].name%>" : <%if(attributes[i].type == 'integer'){%>1<%}else{%>"<%=attributes[i].name%>"<%}%><%if(i < attributes.length - 1 ){%>,<%}%>
+    <% for(var i in attributes) { %>"<%=attributes[i].name%>" : <%if(attributes[i].type == 'integer'){%>1<%}else{%><%if(attributes[i].type == 'boolean'){%>true<%}else{%><%if(attributes[i].type == 'array'){%>[]<%}else{%><%if(attributes[i].type == 'json'){%>{}<%}else{%>"<%=attributes[i].name%>"<%}%><%}%><%}%><%}%><%if(i < attributes.length - 1 ){%>,<%}%>
     <% } %>
   }
 ]
